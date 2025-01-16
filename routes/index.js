e6407ae6a6455d48905f95002df4f9fc08360ab4
Ethan Aldrich <ethanaldrich9986@gmail.com>
2025-01-15 17:24:22 -0800

    Added property description page that is generated by the route

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   // Sample property data
   const properties = [
     {
+      id: "1",
       title: "Oceanfront Villa",
       location: "Malibu, California",
       price: "12,500,000",
@@ -15,6 +16,7 @@ router.get('/', function(req, res, next) {
       image: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750"
     },
     {
+      id: "2",
       title: "Modern Penthouse",
       location: "Manhattan, New York",
       price: "8,900,000",
@@ -24,6 +26,7 @@ router.get('/', function(req, res, next) {
       image: "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9"
     },
     {
+      id: "3",
       title: "Mediterranean Estate",
       location: "Beverly Hills, California",
       price: "15,750,000",
@@ -43,6 +46,7 @@ router.get('/properties', function(req, res, next) {
   // Using the same property data for now
   const properties = [
     {
+      id: "1",
       title: "Oceanfront Villa",
       location: "Malibu, California",
       price: "12,500,000",
@@ -52,6 +56,7 @@ router.get('/properties', function(req, res, next) {
       image: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750"
     },
     {
+      id: "2",
       title: "Modern Penthouse",
       location: "Manhattan, New York",
       price: "8,900,000",
@@ -61,6 +66,7 @@ router.get('/properties', function(req, res, next) {
       image: "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9"
     },
     {
+      id: "3",
       title: "Mediterranean Estate",
       location: "Beverly Hills, California",
       price: "15,750,000",
@@ -74,6 +80,48 @@ router.get('/properties', function(req, res, next) {
   res.render('properties', { 
     properties: properties
   });
+});
+
+router.get('/property/:id', function(req, res, next) {
+  // In a real app, you would fetch this from a database using req.params.id
+  // For now, we'll use sample data
+  const properties = [
+    {
+      id: "1",
+      title: "Oceanfront Villa",
+      location: "Malibu, California",
+      price: "12,500,000",
+      beds: 6,
+      baths: 8,
+      sqft: "8,500",
+      image: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750",
+      description: "Spectacular oceanfront villa offering breathtaking views of the Pacific. This luxurious residence features high-end finishes throughout, a gourmet kitchen with top-of-the-line appliances, and an infinity pool overlooking the ocean.",
+      features: [
+        "Infinity Pool",
+        "Wine Cellar",
+        "Home Theater",
+        "Private Beach Access",
+        "Smart Home Technology",
+        "4-Car Garage"
+      ],
+      additionalImages: [
+        "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9",
+        "https://images.unsplash.com/photo-1600585154340-be6161a56a0c",
+        "https://images.unsplash.com/photo-1600607687939-ce8a6c25118c"
+      ],
+      agent: {
+        name: "Sarah Johnson",
+        phone: "+1 (310) 555-0123",
+        email: "sarah@12mgt.com",
+        image: "https://images.unsplash.com/photo-1560250097-0b93528c311a"
+      }
+    }
+    // Add more properties as needed
+  ];
+
+  const property = properties.find(p => p.id === req.params.id) || properties[0];
+  
+  res.render('property-details', { property });
 });
 
 module.exports = router;
