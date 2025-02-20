ea29e9df7f92c1e546d118ae2ed769f47ed491f1
Ethan Aldrich <e.aldrich@budgetlasvegas.com>
2025-02-20 11:59:47 -0800

    Updated TLS

--- config/mailer.js
+++ config/mailer.js
@@ -9,12 +9,21 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    ciphers: 'TLS_AES_256_GCM_SHA384:TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:ECDHE-RSA-AES128-GCM-SHA256',
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  },
+  pool: true,
+  maxConnections: 3,
+  maxMessages: 100
 });
 
 // Verify transporter
 transporter.verify(function (error, success) {
   if (error) {
     console.log('Error verifying mail server:', error);
+    console.log('Detailed error:', error.message);
   } else {
     console.log('Mail server is ready to send messages');
   }
